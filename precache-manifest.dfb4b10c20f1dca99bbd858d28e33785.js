836c6ee6d1f8b59250e6ecb290da62cf819c60e8
Daniel Mendoza <dmendoza@fb.com>
2019-08-21 17:29:06 -0700

    Updates

---
+++ precache-manifest.dfb4b10c20f1dca99bbd858d28e33785.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a292d18c37f8d0e1c6aae952c6912302",
+    "url": "/study-cards/index.html"
+  },
+  {
+    "revision": "06aff9badf75b1faa6b1",
+    "url": "/study-cards/static/css/main.38dea7da.chunk.css"
+  },
+  {
+    "revision": "79b8ca8376ca8a4ff75d",
+    "url": "/study-cards/static/js/2.d0690dff.chunk.js"
+  },
+  {
+    "revision": "06aff9badf75b1faa6b1",
+    "url": "/study-cards/static/js/main.13b4c3f2.chunk.js"
+  },
+  {
+    "revision": "2ff19a37d84aed5e1e75",
+    "url": "/study-cards/static/js/runtime~main.1577e63c.js"
+  }
+]);
